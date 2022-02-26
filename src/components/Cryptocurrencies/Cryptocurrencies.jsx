772c00d356c2668888edda07aa25db60ccd68ac3
yabandzhiev <yabandzhiev@abv.bg>
2022-02-26 15:28:36 +0200

    add alt tag to img

--- src/components/Cryptocurrencies/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies/Cryptocurrencies.jsx
@@ -38,7 +38,13 @@ const Cryptocurrencies = ({ simplified }) => {
             <Link to={`/crypto/${currency.uuid}`}>
               <Card
                 title={`${currency.rank}. ${currency.name}`}
-                extra={<img className="crypto-image" src={currency.iconUrl} />}
+                extra={
+                  <img
+                    className="crypto-image"
+                    src={currency.iconUrl}
+                    alt="Crypto avatar"
+                  />
+                }
                 hoverable
               >
                 <p>Price: ${millify(currency.price)}</p>
