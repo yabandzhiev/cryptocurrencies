0cec521e41ed57664fd1d033e665a03e504a07d5
yabandzhiev <yabandzhiev@abv.bg>
2022-02-26 15:28:48 +0200

    comment out exchanges page

--- src/components/Exchanges/Exchanges.jsx
+++ src/components/Exchanges/Exchanges.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import millify from "millify";
-import { Collapse, Row, Col, Typography, Avatar } from "antd";
-import HTMLReactParser from "html-react-parser";
+// import React from "react";
+// import millify from "millify";
+// import { Collapse, Row, Col, Typography, Avatar } from "antd";
+// import HTMLReactParser from "html-react-parser";
 
-import { useGetExchangesQuery } from "../../services/cryptoApi";
-import Loader from "../Loader/Loader.jsx";
+// import { useGetExchangesQuery } from "../../services/cryptoApi";
+// import Loader from "../Loader/Loader.jsx";
 
-const { Text } = Typography;
-const { Panel } = Collapse;
+// const { Text } = Typography;
+// const { Panel } = Collapse;
 
 const Exchanges = () => {
   // const { data, isFetching } = useGetExchangesQuery();
